4ff1813d3af734a6772e7bd13973c500ed21259d
asus <asus@DESKTOP-JDFRGUU>
2019-04-02 15:34:54 +0200

    web

---
+++ ClientDeTest/src/main/java/tn/esprit/HelloServiceClient.java
@@ -0,0 +1,49 @@
+package tn.esprit;
+ 
+import java.util.List;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+
+import entities.Course;
+import entities.User;
+import services.Register;
+import services.RegisterRemote;
+  
+public class HelloServiceClient { 
+ 
+	public static void main(String[] args) throws NamingException {
+		String jndiName="spectrum-ear/spectrum-ejb/Register!services.RegisterRemote";
+
+		try
+		{
+		Context context = new InitialContext();  
+		RegisterRemote proxy=(RegisterRemote) context.lookup(jndiName);
+		User e= new User();
+		Course c= new Course();
+		e=proxy.getUserDetails(4);
+		e.setEmail("kkkk");
+		e.setUsername("kkkk"); 
+		e.setPassword("kkkk"); 
+
+		//proxy.createCoach(e);
+		//proxy.updateparameter("nnnnn",4);
+          proxy.login("coach" , "coach");
+		
+  		List<Course> detailsList = proxy.findAll();
+  		System.out.println(detailsList);
+		
+		//c.setDescription("ooooo");
+
+		//proxy.deletecourse(1);
+		//proxy.updateUserDetails(e);
+
+		}
+	catch(NamingException e )
+	{
+		e.printStackTrace();
+	}
+	}
+} 
+ 
